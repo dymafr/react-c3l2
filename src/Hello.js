98e7aa9f7d690098735427dca1bfd5ce3bdd3b66
dymafr <erwan1309@hotmail.com>
2022-06-08 11:45:47 +0200

    2 files modified

--- src/Hello.js
+++ src/Hello.js
@@ -1,3 +1,7 @@
-export default function Hello() {
+export function Hello() {
   return <h1>Hello World!</h1>;
+}
+
+export function Greeting() {
+  return <h2>Greetings</h2>;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Hello from './Hello';
+import { Hello, Greeting } from './Hello';
 
 const root = createRoot(document.getElementById('root'));
 
